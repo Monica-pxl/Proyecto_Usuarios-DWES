8a463915e9c17831db0089846f32d00c7e550c5e
Monica-pxl <tu_correo_de_github@example.com>
2026-01-29 11:48:43 +0100

    chat controller eliminado

--- src/Controller/ChatController.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Este archivo ha sido desactivado
-// Todas las rutas /api/chat/* han sido migradas a ApiAuthController.php
-// Las funcionalidades han sido reorganizadas según la documentación
